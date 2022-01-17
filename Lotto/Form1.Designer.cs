9a9e330003df39c8c923a1f00252186d7a7d8849
ben-lawlor-dkit <d00234161@student.dkit.ie>
2022-01-17 15:56:11 +0000

    Update

---
+++ Lotto/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Lotto
+{
+    partial class Lotto
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lotto));
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.AscendingCheckbox = new System.Windows.Forms.CheckBox();
+            this.numbersListBox = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.GenerateButton.BackColor = System.Drawing.SystemColors.Menu;
+            this.GenerateButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.GenerateButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(90)))), ((int)(((byte)(106)))));
+            this.GenerateButton.Location = new System.Drawing.Point(319, 677);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(816, 107);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Generate Random Lottery Numbers!";
+            this.GenerateButton.UseVisualStyleBackColor = false;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // AscendingCheckbox
+            // 
+            this.AscendingCheckbox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.AscendingCheckbox.AutoSize = true;
+            this.AscendingCheckbox.Location = new System.Drawing.Point(85, 700);
+            this.AscendingCheckbox.Name = "AscendingCheckbox";
+            this.AscendingCheckbox.Size = new System.Drawing.Size(172, 54);
+            this.AscendingCheckbox.TabIndex = 2;
+            this.AscendingCheckbox.Text = "Organise In \r\nAscending Order";
+            this.AscendingCheckbox.UseVisualStyleBackColor = true;
+            this.AscendingCheckbox.CheckedChanged += new System.EventHandler(this.AscendingCheckbox_CheckedChanged);
+            // 
+            // numbersListBox
+            // 
+            this.numbersListBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.numbersListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numbersListBox.Font = new System.Drawing.Font("Bahnschrift Condensed", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numbersListBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(222)))), ((int)(((byte)(64)))));
+            this.numbersListBox.FormattingEnabled = true;
+            this.numbersListBox.ItemHeight = 115;
+            this.numbersListBox.Location = new System.Drawing.Point(470, 41);
+            this.numbersListBox.Name = "numbersListBox";
+            this.numbersListBox.Size = new System.Drawing.Size(951, 115);
+            this.numbersListBox.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1307, 606);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CloseButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(90)))), ((int)(((byte)(106)))));
+            this.CloseButton.Location = new System.Drawing.Point(1309, 773);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(112, 34);
+            this.CloseButton.TabIndex = 8;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // Lotto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1433, 819);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.numbersListBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.AscendingCheckbox);
+            this.Controls.Add(this.GenerateButton);
+            this.Name = "Lotto";
+            this.Text = "Lotto";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.CheckBox AscendingCheckbox;
+        private System.Windows.Forms.ListBox numbersListBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
+
---
+++ Lotto/Form1.cs
@@ -0,0 +1,121 @@
+﻿/* Ben Lawlor 2020
+DkIT Higher Diploma in Science in Computing */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lotto
+{
+    public partial class Lotto : Form
+    {
+        //2 integer arrays, 
+        //one for displaying the numbers in ascending order
+        //and another for the order they were generated in.
+        public int[] lotto = new int[6];
+        public int[] unorganized = new int[6];
+
+        int entries = 0;
+
+        public Lotto()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            //using a for-loop to generate 6 random lottery numbers between 1 and 47
+            numbersListBox.Items.Clear();
+            Random random = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                int number = random.Next(1, 48);
+
+                //if the lotto array already has this number, we don't add it
+                //and we run the loop an extra time
+                if (lotto.Contains(number))
+                {
+                    i--;
+                }
+
+                //otherwise we add it to both arrays
+                else
+                {
+                    lotto[i] = number;
+                    unorganized[i] = number;
+                }
+            }
+
+            //If the ascending order checkbox is checked before we click generate,
+            //the numbers will automatically be organised.
+            //However we can still see the original order by unchecking the box
+            //because we have used the two arrays.
+            if (AscendingCheckbox.Checked)
+            {
+                Array.Sort(lotto);
+            }
+            
+            //We use this method to clean up and then populate the numbers listbox
+            //in a centered format
+            PopulateNumbersListBox(lotto);
+
+            entries++;
+            if (entries % 7 == 0)
+            {
+                MessageBox.Show("You won!", "Congratulations");
+            }
+        }
+
+        private void AscendingCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+             if (AscendingCheckbox.Checked && lotto[0] != 0) 
+            {
+                //If the box to organise the numbers is checked, and if numbers have been generated,
+                //we sort the array, clear the list box and re-print the array with its new order.
+                Array.Sort(lotto);
+                PopulateNumbersListBox(lotto);
+            }
+
+             if (AscendingCheckbox.Checked == false && lotto[0] != 0) 
+            {
+                //If the box is unchecked, we print the numbers in the order that they were generated
+                PopulateNumbersListBox(unorganized);                
+            }
+        }
+
+        private void PopulateNumbersListBox(int[] numbers)
+        {
+            //We clear and then send the lotto numbers to the list box 
+            //as a single string with some spaces between each element, to display them
+            //in a single line.
+            //These if, else if & else statements do their best to keep the numbers centered.
+
+            numbersListBox.Items.Clear();
+            if (numbers[0] >= 10)
+            {
+                numbersListBox.Items.Add(string.Format("  {0}   {1}   {2}   {3}   {4}   {5}   ", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]));
+            }
+            else if (numbers[0] < 10 && numbers[1] < 10)
+            {
+                numbersListBox.Items.Add(string.Format("    {0}   {1}   {2}   {3}   {4}   {5}   ", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]));
+            }
+            else
+            {
+                numbersListBox.Items.Add(string.Format("   {0}   {1}   {2}   {3}   {4}   {5}   ", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5]));
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
